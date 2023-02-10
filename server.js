736f8e1c2b91c51f09693bd5ea2001060a026529
Ameya Bhagat <Bhagatameya21@gmail.com>
2023-02-10 15:46:03 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const http = require('http');
+const express = require("express");
+
+
+const app = express();
+const server = http.createServer();
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname,"public")))
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () =>
+  console.log(`Server Running on http://localhost:${PORT}`)
+);
